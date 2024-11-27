e4e12d24b7564d025af414fd1311b0782d119468
Ashein00 <ashen.induwara@hotmail.com>
2024-11-27 14:49:29 +0530

    All good for create journal entry instruction.

--- anchor/programs/cruddapp/src/lib.rs
+++ anchor/programs/cruddapp/src/lib.rs
@@ -8,5 +8,36 @@ declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 pub mod cruddapp {
     use super::*;
 
+    pub fn create_journal_entry(ctx: Contex<CreateEntry>, title: String,) -> Result<()> {
+
+    }
+
 }
 
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct CreateEntry<'info> {
+    #[account(
+        init, 
+        seed = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        space = 8 + JournalEntryState::INIT_SPACE,
+        payer = owner,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct JournalEntryState {
+    pub owner: Pubkey,
+    #[max_len(50)]
+    pub title: String,
+    #[max_len(1000)]
+    pub message: String,
+}
